4cffa527f4abd7cc1666e666af27139dcc77f3d9
Matthew Ogtong <ogtongm@gmail.com>
2023-01-22 11:43:20 -0500

    Add new color extension and TabView

---
+++ DogGram/Helpers/Extensions.swift
@@ -0,0 +1,29 @@
+//
+//  Extensions.swift
+//  DogGram
+//
+//  Created by Matthew Ogtong on 1/22/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    struct MyTheme {
+        
+        static var purpleColor: Color {
+            return Color("ColorPurple")
+        }
+        
+        static var yellowColor: Color {
+            return Color("ColorYellow")
+        }
+        
+        static var beigeColor: Color {
+            return Color("ColorBeige")
+        }
+        
+    }
+    
+}
