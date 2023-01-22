4ef516796981f46cb1cf36de2a18838fc484eb01
Matthew Ogtong <ogtongm@gmail.com>
2023-01-21 19:28:04 -0500

    Initial Commit

---
+++ DogGram/DogGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogGramApp.swift
+//  DogGram
+//
+//  Created by Matthew Ogtong on 1/21/23.
+//
+
+import SwiftUI
+
+@main
+struct DogGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
