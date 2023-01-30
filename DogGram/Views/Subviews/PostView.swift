d2be3976d65b948d5786558244ec375d4e0118df
Matthew Ogtong <ogtongm@gmail.com>
2023-01-29 19:26:10 -0500

    Add post action sheet with reporting and sharing options in PostView screen

--- DogGram/Views/Subviews/PostView.swift
+++ DogGram/Views/Subviews/PostView.swift
@@ -9,9 +9,17 @@ import SwiftUI
 
 struct PostView: View {
     
+    enum PostActionSheetOption {
+        case general
+        case reporting
+    }
+    
     @State var post: PostModel
+    @State var postImage: UIImage = UIImage(named: "nimbus2")!
     @State var animateLike: Bool = false
     @State var addHeartAnimationToView: Bool
+    @State var showActionSheet: Bool = false
+    @State var actionSheetType: PostActionSheetOption = .general
     
     var showHeaderAndFooter: Bool
     
@@ -39,15 +47,24 @@ struct PostView: View {
                     
                     Spacer()
                     
-                    Image(systemName: "ellipsis")
-                        .font(.headline)
+                    Button {
+                        showActionSheet.toggle()
+                    } label: {
+                        Image(systemName: "ellipsis")
+                            .font(.headline)
+                    }
+                    .foregroundColor(.primary)
+                    .actionSheet(isPresented: $showActionSheet) {
+                        getActionSheet()
+                    }
+                    
                 }
                 .padding(.all, 6)
             }
             
             // MARK: IMAGE
             ZStack {
-                Image("nimbus2")
+                Image(uiImage: postImage)
                     .resizable()
                     .scaledToFit()
                 
@@ -81,8 +98,13 @@ struct PostView: View {
                             .foregroundColor(.primary)
                     }
                     
-                    Image(systemName: "paperplane")
-                        .font(.title3)
+                    Button {
+                        sharePost()
+                    } label: {
+                        Image(systemName: "paperplane")
+                            .font(.title3)
+                    }
+                    .foregroundColor(.primary)
                     
                     Spacer()
                 }
@@ -121,6 +143,68 @@ struct PostView: View {
         let updatedPost = PostModel(postID: post.postID, userID: post.userID, username: post.username, caption: post.caption, dateCreated: post.dateCreated, likeCount: post.likeCount - 1, likedByUser: false)
         self.post = updatedPost
         
+    }
+    
+    func getActionSheet() -> ActionSheet {
+        
+        switch self.actionSheetType {
+        case .general:
+            return ActionSheet(title: Text("Where would you like to go?"), message: nil, buttons: [
+                .destructive(Text("Report"), action: {
+                    self.actionSheetType = .reporting
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                        self.showActionSheet.toggle()
+                    }
+                }),
+                .default(Text("Learn more..."), action: {
+                    print("LEARN MORE PRESSED")
+                }),
+                .cancel()
+            ])
+        case .reporting:
+            return ActionSheet(title: Text("Why are you reporting this post?"), message: nil, buttons: [
+                
+                .destructive(Text("This is inappropriate"), action: {
+                    reportPost(reason: "This is inappropriate")
+                }),
+                .destructive(Text("This is spam"), action: {
+                    reportPost(reason: "This is spam")
+                }),
+                .destructive(Text("It made me uncomfortable"), action: {
+                    reportPost(reason: "It made me uncomfortable")
+                }),
+                .cancel({
+                    self.actionSheetType = .general
+                })
+                
+            ])
+        }
+        
+    }
+    
+    func reportPost(reason: String) {
+        print("REPORT POST NOW")
+    }
+    
+    func sharePost() {
+        
+        let message = "Check out this post on DogGram!"
+        let image = postImage
+        let link = URL(string: "https://www.google.com")!
+        
+        let activityViewController = UIActivityViewController(activityItems: [message, image, link], applicationActivities: nil)
+        
+        guard let firstScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
+            return
+        }
+        
+        guard let firstWindow = firstScene.windows.first else {
+            return
+        }
+        
+        let viewController = firstWindow.rootViewController
+        
+        viewController?.present(activityViewController, animated: true, completion: nil)
     }
     
 }
